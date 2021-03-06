69e82347f3df1133f5d320c59436cb73683f41a6
pdd27673 <paul@Pauls-MacBook-Air.local>
2021-03-06 15:00:32 -0800

    Fetching student with grades less than 60%

--- app/js/API.js
+++ app/js/API.js
@@ -14,22 +14,50 @@ var firebaseConfig = {
     firebase.initializeApp(firebaseConfig);
     firebase.analytics();
 
+//database var
 var db = firebase.firestore();
 
+//Main variables
+const teacherCollection = db.collection('Users').doc('Teachers').collection('TeacherInfo');
+var teacherID = [];
+var grades = [];
 
+//Fetch every teachers
+//store in teacherID array
 async function fetchData() {
-    const teacherCollection = db.collection('Users').doc('Teachers').collection('TeacherInfo');
     const teacherInfo = await teacherCollection.get();
-
     teacherInfo.forEach((doc => {
-        console.log(doc.data());
-    }))
+        teacherID.push(doc.id);
+    }));
 }
 
-// function fetchStudentInfo(id) {
-//     const teacherCollection = db.collection('Users').doc('Teachers').collection('TeacherInfo');
-//     const studentInfo = teacherCollection.collection(`${id}`).doc('StudentInfo');
-//     studentInfo.get().then((doc) => {
-//         console.log(doc.id, doc.data());
-//     });
-// }
+//Fetch every ID in teacherID
+//For student information
+//Store info of < 60% in variable
+async function fetchStudentData() {
+    teacherID.forEach(async function (id) {
+        const studentCollection = teacherCollection.doc(`${id}`).collection('StudentInfo');
+        const studentInfo = await studentCollection.get();
+
+        //for each ID pull up corresponding student
+        studentInfo.forEach(async function (doc)  {
+            let name = doc.get("lastName");
+            let student = []
+            student.push(name)
+            const quizCollection = studentCollection.doc(`${doc.id}`).collection('QuizInfo');
+            const quizInfo = await quizCollection.get();
+
+            quizInfo.forEach((quiz => {
+                    student.push(quiz.get('grade'))
+            }))
+            if(student[1] <= .6 || student[2] <= .6 ){
+                grades.push(student)
+            }
+        });
+        
+    });
+
+    grades.forEach((entry => {
+        console.log(entry);
+    }))
+}
